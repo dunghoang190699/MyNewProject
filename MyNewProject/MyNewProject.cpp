f719233bcd2ed418fa7481027bfe5ff878930729
Hoang Dung <dung.hoang190699@gmail.com>
2022-02-09 10:27:22 +0700

    test commit

--- MyNewProject/MyNewProject.cpp
+++ MyNewProject/MyNewProject.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    std::cout << "conac" << std::endl;
+    std::cout << "conac" << std::endl; 
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
